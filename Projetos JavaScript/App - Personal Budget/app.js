a2230da0a95c197ca261b63373d0bdd9dc92c081
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2023-03-12 00:39:44 -0300

    :hammer: improvement in data query
    perf: Included improvements to the visualization of the inserted expense data query, making the deployed data more clearly visible to the user.

--- Projetos JavaScript/App - Personal Budget/app.js
+++ Projetos JavaScript/App - Personal Budget/app.js
@@ -64,6 +64,51 @@ class Bd {
 
         return expenses
     }
+
+    servey(expense) {
+
+       let expensesFiltered = Array()
+
+       expensesFiltered = this.loadAllInserts()
+
+       console.log(expensesFiltered)
+
+       //year
+       if(expense.year !='') {
+        console.log('filter of year')
+        expensesFiltered = expensesFiltered.filter(d => d.year == expense.year)
+       }
+
+       //mounth
+       if(expense.mounth !='') {
+        console.log('filter of mounth')
+        expensesFiltered = expensesFiltered.filter(d => d.mounth == expense.mounth)
+       }
+
+       //day
+       if(expense.day !='') {
+        console.log('filter of day')
+        expensesFiltered = expensesFiltered.filter(d => d.day == expense.day)
+       }
+
+       //type
+       if(expense.type !='') {
+        console.log('filter of type')
+        expensesFiltered = expensesFiltered.filter(d => d.type == expense.type)
+       }
+
+       //description
+       if(expense.description !='') {
+        console.log('filter of description')
+        expensesFiltered = expensesFiltered.filter(d => d.description == expense.description)
+       }
+
+       //amount
+       if(expense.amount !='') {
+        console.log('filter of amount')
+        expensesFiltered = expensesFiltered.filter(d => d.amount == expense.amount)
+       }
+    }
 }
 
 let bd = new Bd()
@@ -90,9 +135,18 @@ function registerCharges() {
     document.getElementById('modal_btn').className = 'btn btn-success'
     //sucess dialog
     $('#insertExpense').modal('show')
-    } else{
-        //erro dialog
+    
+    //In case of the user didn't completed to filled the fields, it will prevent to clean it and start over
 
+    year.value = ''
+    mounth.value = ''
+    day.value = ''
+    type.value = ''
+    description.value = ''
+    amount.value = ''
+    } else{
+
+    //erro dialog
     document.getElementById('modal_title').innerHTML = 'Data insertion error'
     document.getElementById('modal_title_div').className = 'modal-header text-sucess'
     document.getElementById('modal_content').innerHTML = 'Check that all field are filled correctly'
@@ -109,5 +163,56 @@ function loadExpenseList(){
 
     expenses = bd.loadAllInserts()
 
-    console.log(expenses)
+    //selection tbody's array elements
+    let expensesList = document.getElementById('expensesList')
+
+    /* 
+    <tr>
+        <th>Data</th>
+        <th>Type</th>
+        <th>Description</th>
+        <th>Amount</th>
+        <th></th>
+    </tr>
+    */
+   //navegate expanses array, listing each expense in a dynamic way
+   expenses.forEach(function(d){
+
+    //create the lines (tr)
+    let line = expensesList.insertRow()
+
+    //create the collums(td)
+    line.insertCell(0).innerHTML = `${d.day}/${d.mounth}/${d.year}`  
+
+    //type ajustment
+    switch(d.type){
+        case '1': d.type = 'Feeding'
+            break
+        case '2': d.type = 'Education'
+            break
+        case '3': d.type = 'Leisure'
+            break
+        case '4': d.type = 'Health'
+            break
+        case '5': d.type = 'Transportation'
+            break
+    }
+    line.insertCell(1).innerHTML = d.type
+
+    line.insertCell(2).innerHTML = d.description
+    line.insertCell(3).innerHTML = d.amount
+   })
+}
+
+function serveyExpense(){
+    let year = document.getElementById('year').value
+    let mounth = document.getElementById('mounth').value
+    let day = document.getElementById('day').value 
+    let type = document.getElementById('type').value
+    let description = document.getElementById('description').value
+    let amount = document.getElementById('amount').value
+
+    let expense = new Expense (year, mounth, day, type, description, amount)
+
+    bd.servey(expense)
 }
